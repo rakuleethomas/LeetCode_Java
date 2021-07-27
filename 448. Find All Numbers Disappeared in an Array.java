1596474e49e0b3fbc1e96f8100f7dde7aa795bf9
Donghun Thomas Han <thomas@rakulee.org>
2021-07-26 22:59:32 -0700

    Create 448. Find All Numbers Disappeared in an Array.java

---
+++ 448. Find All Numbers Disappeared in an Array.java
@@ -0,0 +1,63 @@
+///////////////////////////////////////////////////////////////
+//  Solution 1.      Using Hash Map
+//  Time Complexity  O(N)
+//  Space Complexity O(N)
+///////////////////////////////////////////////////////////////
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        HashMap<Integer, Boolean> hashMap = new HashMap<Integer, Boolean>();
+        
+        for(int i=0; i<nums.length; i++){
+            hashMap.put(nums[i], true);
+        }
+        
+        List<Integer> result = new LinkedList<Integer>();
+        
+        for(int i=1; i<=nums.length; i++){
+            if(!hashMap.containsKey(i)){
+                result.add(i);
+            }
+        }
+        
+        return result;
+    }
+}
+
+///////////////////////////////////////////////////////////////
+//  Solution 2.      Space InPlace Modification Solution
+//  Time Complexity  O(N)
+//  Space Complexity O(1)
+///////////////////////////////////////////////////////////////
+// class Solution {
+//     public List<Integer> findDisappearedNumbers(int[] nums) {
+        
+//         // Iterate over each of the elements in the original array
+//         for (int i = 0; i < nums.length; i++) {
+            
+//             // Treat the value as the new index
+//             int newIndex = Math.abs(nums[i]) - 1;
+            
+//             // Check the magnitude of value at this new index
+//             // If the magnitude is positive, make it negative 
+//             // thus indicating that the number nums[i] has 
+//             // appeared or has been visited.
+//             if (nums[newIndex] > 0) {
+//                 nums[newIndex] *= -1;
+//             }
+//         }
+        
+//         // Response array that would contain the missing numbers
+//         List<Integer> result = new LinkedList<Integer>();
+        
+//         // Iterate over the numbers from 1 to N and add all those
+//         // that have positive magnitude in the array
+//         for (int i = 1; i <= nums.length; i++) {
+            
+//             if (nums[i - 1] > 0) {
+//                 result.add(i);
+//             }
+//         }
+        
+//         return result;
+//     }
+// }
