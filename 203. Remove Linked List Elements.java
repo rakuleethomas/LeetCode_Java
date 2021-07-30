53f2757a59af197be1b086f3ce46dc36a2c1932c
Donghun Thomas Han <thomas@rakulee.org>
2021-07-30 09:24:24 -0700

    203. Remove Linked List Elements

---
+++ 203. Remove Linked List Elements.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+///////////////////////////////////////////////////////////////
+//  Solution 1.      Using Sentinel Node
+//  Time Complexity  O(N)
+//  Space Complexity O(1)
+///////////////////////////////////////////////////////////////
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode curr = head;
+        
+        while(curr != null){
+            if(curr.val == val){
+                prev.next = curr.next;
+            }else{
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        
+        return dummy.next;
+    }
+}
